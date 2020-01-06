e86b1bdf1d1660e8bc67167219fbe4af47c12d1f
Rumour mill <ld@itu.dk>
2020-01-06 13:11:59 +0100

    add store debug info, adapt find_rumour signature

--- mill.py
+++ mill.py
@@ -2,21 +2,23 @@
 
 from rm import RumourMill
 
+debug = True
+
 mill = RumourMill()
 
 mill.load_rumours('data/')
+if debug:
+    print(mill.rumour_store.keys())
 mill.init_pi()
 mill.init_printer()
 
-debug = True
-
-while true:
+while True:
     if not debug:
         mill.wait_for_activation()
     params = mill.capture_params()
     if debug:
         print(params)
-    rumour = mill.find_rumour(params)
+    rumour = mill.find_rumour(params['genre'], params['tense'], params['temperature'])
     mill.print_rumour(rumour)
     if debug:
         break
