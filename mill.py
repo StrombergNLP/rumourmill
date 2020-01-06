7aed594f70efe534b53667b7604fa9def73f5161
Rumour mill <ld@itu.dk>
2020-01-06 12:12:05 +0100

    add debug mode

--- mill.py
+++ mill.py
@@ -4,12 +4,19 @@ from rm import RumourMill
 
 mill = RumourMill()
 
-mill.load_rumours('rumours/')
+mill.load_rumours('data/')
 mill.init_pi()
 mill.init_printer()
 
+debug = True
+
 while true:
-	mill.wait_for_activation()
-	params = mill.capture_params()
-	rumour = mill.find_rumour(params)
-	mill.print_rumour(rumour)
+    if not debug:
+        mill.wait_for_activation()
+    params = mill.capture_params()
+    if debug:
+        print(params)
+    rumour = mill.find_rumour(params)
+    mill.print_rumour(rumour)
+    if debug:
+        break
